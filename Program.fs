076a14688bcb0e824502e161ca67f47cdb92b4b3
John Stovin <john.stovin@3squared.com>
2019-02-12 08:50:24 +0000

    Initial audio output

--- Program.fs
+++ Program.fs
@@ -1,8 +1,25 @@
 ﻿// Learn more about F# at http://fsharp.org
 
-open System
+open AudioTypes
+open AudioOut
+open NAudio.Wave
+open System.Threading
+
+let silenceGenerator state = Some (0.0, state)
+let silence = Seq.unfold silenceGenerator ()
+let stream = {data = silence; sampleRate = 44100}
+let bundle = [stream]
 
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
+    printfn "Starting playback"
+
+    let out = new WaveOutEvent()
+    out.Init(new StreamProvider(bundle))
+    out.Play()
+    Thread.Sleep(1000)
+    out.Stop()
+
+    printfn "Playback finished"
+
     0 // return an integer exit code
